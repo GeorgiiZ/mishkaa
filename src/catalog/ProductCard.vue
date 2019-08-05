21c3e5fde2b771d31d194fb5223e2f62194c0e47
GZennurov <zwgybds@yahoo.com>
2019-08-05 22:14:46 +0300

    Fix catalog styling

--- src/catalog/ProductCard.vue
+++ src/catalog/ProductCard.vue
@@ -1,19 +1,18 @@
 <template>
     <div class="product">
         <img class="product__img" :src="product.src"/>
-        <div class="product__desc">
-            <div class="product__desc-name" ><strong>{{product.name}}</strong></div>
-            <span v-for="(param, key) in product.parameters" :key="key">
-                {{ key | mapParamName }} {{ param | mapUnit(key)}},
-            </span>
-        </div>
-        <div class="product__buy">
-            <hr>
-            <div class="product__buy-cost">
-                <strong>{{product.cost}} руб.</strong>
+        <div class="product__info"
+            @click="basketClick()">
+            <div class="product__desc">
+                <div class="product__desc-name" >{{product.name}}</div>
+                <span v-for="(param, key) in product.parameters" :key="key">
+                    {{ key | mapParamName }} {{ param | mapUnit(key)}},
+                </span>
+            </div>
+            <div class="product__buy">
+                <span class="product__buy-cost">{{product.cost}} руб.</span>
                 <img class="product__buy-basket" 
-                     src="../assets/cartsvg.png"
-                     @click="basketClick()"/>
+                     src="../assets/cartsvgcopy.png"/>
             </div>
         </div>
     </div>
@@ -42,46 +41,63 @@ export default {
     font-size: var(--text-size__light);
     display: flex;
     flex-direction: column;
+    max-width: 316px;
 }
 
-.product__img {
-    margin: 0 auto;
-}
 
+.product__info {
+    display: flex;
+    flex-direction: column;
+    padding: 30px 30px 0px 30px;
+    cursor: pointer;
+    flex: 1;
+}
 
 .product__desc {
     max-width: 315px;
-    padding: 30px 30px 0px 30px;
     line-height: var(--line-height__paragraph);
+    margin-bottom: 10px; 
 }
 
 .product__desc-name {
     font-size: var(--text-size__regular);
     font-weight: bold;
+    margin-bottom: 5px; 
 }
 
 .product__buy {
-    padding: 0 30px 30px 30px;
-    margin-top: auto
+    margin-top: auto; 
+    padding-top: 15px; 
+    border-top: 0.5px solid black;
+    display: flex;
+    align-items: center;
 }
 
 .product__buy-cost {
-    display: flex;
-    align-items: center;
+    font-weight: bold;
 }
 
 .product__buy-basket {
   max-height: 25px;
   position: relative;
   margin-left: auto;
+  opacity: 0.2;
 }
 
-.product__buy-basket:hover {
-    cursor: pointer;
+.product__info:hover .product__buy-basket {
+    opacity: 1;
 }
 
-hr {
-    border: 0.5px solid black;
+.product__info:hover .product__desc-name {
+    opacity: 0.6;
+}
+
+.product__info:active .product__buy-basket {
+    opacity: 0.3;
+}
+
+.product__info:active .product__desc-name {
+    opacity: 0.4;
 }
 
 </style>
